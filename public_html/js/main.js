37e78d4ab870cca66fb0de6ab9f82340074cda48
Vladan Dzulovic <vladan.dzulovic@cubes.rs>
2023-03-23 15:14:55 +0100

    finished

---
+++ public_html/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+    Fancybox.bind("[data-fancybox]", {
+        // Your custom options
+    });
+
+    if ($('.contact-form').length > 0) {
+        $(function () {
+            $(".contact-form").validate({
+                highlight: function (element) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                    $(element).closest('.form-group').addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function (element) {
+                    $(element).removeClass('is-invalid').addClass('is-valid');
+                    $(element).closest('.form-group').addClass("is-valid").removeClass("is-invalid");
+                },
+                rules: {
+                    name: {
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    message:{
+                        required:true
+                    }
+                },
+                errorElement: 'p',
+                errorPlacement: function (error, element) {
+                    error.appendTo($(element).closest('.form-group').find('.invalid-feedback'));
+                }
+
+            });
+        });//Form Validation 
+    }
+    
+    // animation start
+    function animation() {
+        var windowHight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            var pozicija = $(this).offset().top;
+            var animacija = $(this).attr('data-animation');
+            var delay = $(this).attr('data-delay');
+            if (pozicija < scroll + windowHight - 50) {
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animacija);
+            }
+        });
+
+    }
+
+
+
+    animation();
+    
+    $(window).scroll(function(){
+        animation();
+    });
+
+
+});
